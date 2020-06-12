7725478dae00984a707a04d92d2c5db60c6b05f7
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 20:26:52 +0800

    Initialize project

---
+++ src/main/java/com/lambdaschool/com/countrysearch/CountrysearchApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.com.countrysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrysearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountrysearchApplication.class, args);
+    }
+
+}
