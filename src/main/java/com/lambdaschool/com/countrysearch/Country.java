11f8a4a6eaf6b093567e04eb9e37bbba758222f3
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 21:10:19 +0800

    Create Country class

---
+++ src/main/java/com/lambdaschool/com/countrysearch/Country.java
@@ -0,0 +1,72 @@
+package com.lambdaschool.com.countrysearch;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Country {
+    // fields
+    private static final AtomicLong counter = new AtomicLong();
+    private long id;
+    private String name;
+    private long population;
+    private long landMass;
+    private int medianAge;
+
+    public Country(String name, long population, long landMass, int medianAge) {
+        this.id = counter.incrementAndGet();
+        this.name = name;
+        this.population = population;
+        this.landMass = landMass;
+        this.medianAge = medianAge;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    public long getLandMass() {
+        return landMass;
+    }
+
+    public void setLandMass(long landMass) {
+        this.landMass = landMass;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                ", landMass=" + landMass +
+                ", medianAge=" + medianAge +
+                '}';
+    }
+}
