a6ec0b9c82c2a2acc27ccb156313333b5ffddc69
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 21:58:11 +0800

    Instantiate CountryList

--- src/main/java/com/lambdaschool/com/countrysearch/CountrysearchApplication.java
+++ src/main/java/com/lambdaschool/com/countrysearch/CountrysearchApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountrysearchApplication {
 
+    public static CountryList ourCountryList;
+
     public static void main(String[] args) {
+
+        ourCountryList = new CountryList();
         SpringApplication.run(CountrysearchApplication.class, args);
     }
 
