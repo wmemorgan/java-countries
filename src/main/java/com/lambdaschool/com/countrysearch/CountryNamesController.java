d81e47b7851c420b8d9c049baa1d0933ad98c4f5
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 22:38:48 +0800

    Create '/names/start/{letter}' route

--- src/main/java/com/lambdaschool/com/countrysearch/CountryNamesController.java
+++ src/main/java/com/lambdaschool/com/countrysearch/CountryNamesController.java
@@ -3,8 +3,11 @@ package com.lambdaschool.com.countrysearch;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/names")
@@ -22,6 +25,18 @@ public class CountryNamesController {
     }
 
     // /start/{letter}
+    @GetMapping(value = "/start/{letter}",
+    produces = {"application/json"})
+    public ResponseEntity<?> getCountriesByLetter(@PathVariable char letter) {
+        List<Country> countries = CountrysearchApplication
+                .ourCountryList
+                .filterCountries(c ->
+                        c.getName().toLowerCase().charAt(0) == Character.toLowerCase(letter));
+
+        countries.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+
+        return new ResponseEntity<>(countries, HttpStatus.OK);
+    }
 
     // /size/{number}
 }
