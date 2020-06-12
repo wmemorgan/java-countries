f3c2e06e2eaa1c4060ee8822285c2e65ea8c6aa2
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 23:03:28 +0800

    Create '/population/size/{people}' route

---
+++ src/main/java/com/lambdaschool/com/countrysearch/CountryPopulationController.java
@@ -0,0 +1,56 @@
+package com.lambdaschool.com.countrysearch;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/population")
+public class CountryPopulationController {
+    // /size/{number}
+    @GetMapping(value = "/size/{people}",
+            produces = {"application/json"})
+    public ResponseEntity<?> getByPopulationSize(@PathVariable int people) {
+        List<Country> countries = CountrysearchApplication
+                .ourCountryList
+                .filterCountries(c ->
+                        c.getPopulation() >= people);
+
+        countries.sort((c1, c2) -> (int)(c1.getPopulation() - c2.getPopulation()));
+
+        return new ResponseEntity<>(countries, HttpStatus.OK);
+    }
+
+    // /min
+//    @GetMapping(value = "/size/{number}",
+//            produces = {"application/json"})
+//    public ResponseEntity<?> getCountriesByNameLength(@PathVariable int number) {
+//        List<Country> countries = CountrysearchApplication
+//                .ourCountryList
+//                .filterCountries(c ->
+//                        c.getName().length() >= number);
+//
+//        countries.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+//
+//        return new ResponseEntity<>(countries, HttpStatus.OK);
+//    }
+
+    // /max
+//    @GetMapping(value = "/size/{number}",
+//            produces = {"application/json"})
+//    public ResponseEntity<?> getCountriesByNameLength(@PathVariable int number) {
+//        List<Country> countries = CountrysearchApplication
+//                .ourCountryList
+//                .filterCountries(c ->
+//                        c.getName().length() >= number);
+//
+//        countries.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+//
+//        return new ResponseEntity<>(countries, HttpStatus.OK);
+//    }
+}
