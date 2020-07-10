995befd5bd6825322e04b1755fe3ecc3e0937e33
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-10 15:09:43 +0800

    Create the following endpoints: -GET '/names/all' -GET '/names/start/{letter}'

---
+++ countrysearch/src/main/java/com/lambdaschool/countrysearch/controllers/CheckCountry.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.countrysearch.controllers;
+
+import com.lambdaschool.countrysearch.models.Country;
+
+public interface CheckCountry {
+    boolean test(Country c);
+}
---
+++ countrysearch/src/main/java/com/lambdaschool/countrysearch/controllers/CountryController.java
@@ -0,0 +1,56 @@
+package com.lambdaschool.countrysearch.controllers;
+
+import com.lambdaschool.countrysearch.models.Country;
+import com.lambdaschool.countrysearch.repositories.CountryRepository;
+import org.hibernate.annotations.Check;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    CountryRepository countryRepository;
+
+    private List<Country> filterCountries (List<Country> list, CheckCountry tester) {
+        List<Country> tempList = new ArrayList<>();
+
+        for (Country c: list) {
+            if(tester.test(c)) {
+                tempList.add(c);
+            }
+        }
+
+        return tempList;
+    }
+
+    @GetMapping(value="/names/all", produces = "application/json")
+    public ResponseEntity<?> listCountriesByName() {
+        List<Country> myList = new ArrayList<>();
+
+        countryRepository.findAll().iterator().forEachRemaining(myList::add);
+        myList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+
+        return new ResponseEntity<>(myList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> listAllByFirstName(@PathVariable char letter) {
+        List<Country> myList = new ArrayList<>();
+
+        countryRepository.findAll().iterator().forEachRemaining(myList::add);
+        List<Country> filteredList = filterCountries(myList,
+                c -> c.getName().toLowerCase().charAt(0) == Character.toLowerCase(letter));
+
+        filteredList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+
+        return new ResponseEntity<>(filteredList, HttpStatus.OK);
+    }
+}
