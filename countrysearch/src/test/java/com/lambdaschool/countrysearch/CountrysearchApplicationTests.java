5ffcd7a0700cbc214e57f675a59d1ff905608261
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-10 14:14:14 +0800

    Initialize project for web31

---
+++ countrysearch/src/test/java/com/lambdaschool/countrysearch/CountrysearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.countrysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountrysearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
